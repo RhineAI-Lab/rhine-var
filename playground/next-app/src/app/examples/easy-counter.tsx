c4feed8efb2f9d09c4c6d9da81d4fda196c4cf34
HRan2004 <2125764918@qq.com>
2025-01-11 01:06:04 +0800

    fix: do not connect when server side render

--- playground/next-app/src/app/examples/easy-counter.tsx
+++ playground/next-app/src/app/examples/easy-counter.tsx
@@ -5,10 +5,13 @@ enableRhineVarLog(true)
 const defaultValue = {count: 0}
 const url = 'ws://localhost:11600/room-1'
 const state = rhineProxy(defaultValue, url)
+console.log('outside')
 
 function Counter() {
   
   const snap = useRhine(state)
+  console.log('state:', state.json())
+  console.log('snap:', snap)
   
   return <div className='page'>
     <button onClick={() => state.count-- }> - 1 </button>
